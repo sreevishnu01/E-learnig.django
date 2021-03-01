c6396edbc87fd7817c769cb967d448a4c4adb7b6
vishnu <sreevishnu01@gmail.com>
2021-03-01 15:40:24 +0400

    success full part 2

--- courses/views.py
+++ courses/views.py
@@ -10,6 +10,29 @@ from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
 # from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+from django.db.models import Count
+from .models import Subject
+from django.views.generic.detail import DetailView
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        return self.render_to_response({'subjects': subjects,
+                                        'subject': subject,
+                                        'courses': courses})
 
 
 # class ModuleOrderView(CsrfExemptMixin,
@@ -184,5 +207,3 @@ class CourseDeleteView(PermissionRequiredMixin,
     template_name = 'courses/manage/course/delete.html'
     success_url = reverse_lazy('manage_course_list')
     permission_required = 'courses.delete_course'
-
-
